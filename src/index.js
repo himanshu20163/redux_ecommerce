757158707365f7c557a9bdb00b434eeea45eee1f
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-28 01:04:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom'
+import mystore from './Redux/Store';
 import { Provider } from 'react-redux';
-import mystore from './Components/Redux/Store';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <Provider store={mystore}>
+<Provider store={mystore}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
